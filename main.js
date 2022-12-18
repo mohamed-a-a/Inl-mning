19c3ea072086b4569173dbee9a297566b7ed7b9f
mohamed-a-a <mohamed.abdihakim.ali@gmail.com>
2022-12-18 20:43:59 +0100

    commit

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
 
-async function fetchall() {
-    try {
-        const response = await fetch('https://blog-api-assignment.up.railway.app/posts'):
-            let data = response.json():
-
+fetch('https://blog-api-assignment.up.railway.app/posts')
+    .then(res => res.json())
+    .then(data => {
         for (i = 0; i < data.length; i++) {
-            console.log(data[i])
+            console.log(data[i]);
+
+            const blogDiv = document.createElement("div");
+            const blogTitle = document.createElement("H1");
+            const blogContent = document.createElement("p");
+            const blogAuthor = document.createElement("p");
+            const blogTags = document.createElement("p");
+            const blogDate = document.createElement("i");
+
+            const title = document.createTextNode(data[i].title);
+            const content = document.createTextNode(`${data[i].content.slice(0, 100)}`);
+            const author = document.createTextNode(`Author: ${data[i].author}`);
+            const tags = document.createTextNode(`Tags: ${data[i].tags}`);
+            const date = document.createTextNode(`Date: ${data[i].date.slice(0, 10)}`);
+
+
+
+            blogTitle.appendChild(title);
+            blogContent.appendChild(content);
+            blogAuthor.appendChild(author);
+            blogTags.appendChild(tags);
+            blogDate.appendChild(date);
+
+            blogDiv.appendChild(blogTitle)
+            blogDiv.appendChild(blogContent)
+            blogDiv.appendChild(blogAuthor)
+            blogDiv.appendChild(blogTags)
+            blogDiv.appendChild(blogDate)
+
+
+            const element = document.getElementById("div1");
+            element.appendChild(blogDiv);
         }
+    })
 
-
-
-
-
-
-
-    }
-
-    catch (error) {
-        console.log(error)
-
-    }
-}
+/*fetch('https://blog-api-assignment.up.railway.app/posts', {
+method: 'POST',
+headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+},
+body: JSON.stringify({
+    "tags": [
+        "economics, travel, food, favorit, entertainment, games, not sports"
+        ],
+    "_id": "63945656564a1de81f3d0021035a5a",
+    "title": "ljsladf - update",
+    "content": "dsfsadfsdf - update",
+    "author": "jkldfsafsd- update",
+    "date": "2022-12-10T08:58:05.360Z",
+    "__v": 0
+})
+})
+.then(res => res.json())
+.then(data => console.log(JSON.stringify(data)))*/
